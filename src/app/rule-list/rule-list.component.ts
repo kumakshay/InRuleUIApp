48cfdde70761f54588405fccf95cacc0284dc2b6
kumakshay <kumakshay@deloitte.com>
2023-02-21 21:32:07 +0530

    sidebar

---
+++ src/app/rule-list/rule-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rule-list',
+  templateUrl: './rule-list.component.html',
+  styleUrls: ['./rule-list.component.css']
+})
+export class RuleListComponent {
+
+}
